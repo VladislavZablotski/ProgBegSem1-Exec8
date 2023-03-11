27776af419d6802656054b182ad6f2847c9c48ac
unknown <zab_vl@tutby>
2023-03-12 01:17:20 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+int number =Convert.ToInt32(Console.ReadLine());
+
+int count = 2;
+
+while(count<=number)
+{
+    Console.Write(count);
+    Console.Write(" ");
+    count+=2;
+}
